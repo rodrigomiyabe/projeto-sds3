6049bc7a7387a20706cc62a6efd2633ead2f6209
RodrigoMiyabe <rodrigomiyabe@gmail.com>
2021-09-10 11:06:49 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsvendas.services;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Camada que faz acesso ao banco de dados
+@Service
+public class SaleService {
+
+    @Autowired // faz a injecao de dependencia(nao precisa declarar a classe new Seller)
+    private SaleRepository repository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    //Lista todos os Sellers
+    @Transactional(readOnly = true)
+    public Page<SaleDTO> findAll(Pageable pageable){
+        sellerRepository.findAll();
+        Page<Sale> result = repository.findAll(pageable);
+        return result.map(x->new SaleDTO(x));
+    }
+
+}
