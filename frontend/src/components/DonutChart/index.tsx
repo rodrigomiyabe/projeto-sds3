c242c89026e8f773fcf4d83dbd8d41f9d74f97f1
RodrigoMiyabe <rodrigomiyabe@gmail.com>
2021-09-11 15:38:08 -0300

    Barchart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,53 +1,45 @@
 import axios from 'axios';
-import { type } from 'os';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
 
 type ChartData = {
-    series:number[],
-    labels:string[]
+    series: number[],
+    labels: string[]
 }
+
 const DonutChart = () => {
 
-    const [chartData, setChartData] = useState<ChartData>({labels: [], series: []});
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    useEffect(()=> {
+    useEffect(() => {
 
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data =  response.data as SaleSum [];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            setChartData({labels:myLabels, series:mySeries});
-        });
+                setChartData({ labels: myLabels, series: mySeries });
+            });
 
     }, []);
 
-    
-
- //   const mockData = {
-   //     series: [477138, 499928, 444867, 220426, 473088],
-     //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-    
     const options = {
         legend: {
             show: true
         }
     }
-    
+
     return (
         <Chart
-       options = {{...options,labels:chartData.labels }}
-       series = {chartData.series}
-       type = "donut"
-       height = "240"
-      />
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
     );
-  }
-  
-  export default DonutChart;
-  
+}
+
+export default DonutChart;
